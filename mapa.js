995e46b232ebf3f8c958a59640418dbd684712a6
Antonio Tapia <j.anton.tg@gmail.com>
2024-04-25 01:14:03 -0600

    estilos actualizados curosor

--- mapa.js
+++ mapa.js
@@ -1,5 +1,7 @@
+//mapa antes de agregar zoom, jala al centavo 12.03 am
+
 // Variables para mantener el estado de los filtros
-let selectedCategory = "Población en situación de pobreza"; // Valor predeterminado
+let selectedCategory = "Población"; // Valor predeterminado
 let selectedYear = "2020"; // Valor predeterminado
 
 // Carga el GeoJSON y el CSV, y luego inicia la visualización
@@ -13,8 +15,13 @@ Promise.all([
     const projection = d3.geoMercator().fitSize([960, 600], geojson);
     const pathGenerator = d3.geoPath().projection(projection);
 
-    const interpolateColor = t => d3.interpolateRgb("blue", "white")(1 - t);
-    const colorScale = d3.scaleSequential(interpolateColor).domain([0, 100]);
+    let minValue, maxValue;
+    const updateMinMaxValues = () => {
+        const key = `${selectedCategory} ${selectedYear}`;
+        [minValue, maxValue] = d3.extent(csvData, d => parseFloat(d[key]));
+    };
+
+    const colorScale = d3.scaleSequential(d3.interpolateBlues);
 
     const paths = svg.selectAll("path")
         .data(geojson.features)
@@ -23,70 +30,113 @@ Promise.all([
         .attr("d", pathGenerator)
         .attr("class", "mapa");
 
-    // Función para actualizar el mapa con los colores correspondientes a los datos
+    const tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
     function updateMap() {
-        const key = `${selectedCategory} ${selectedYear}`;
+        colorScale.domain([minValue, maxValue]);
+
         paths.transition()
             .duration(500)
             .attr("fill", d => {
                 const cve_mun = d.properties.CVE_MUN;
                 const munData = dataMap.get(cve_mun);
+                const key = `${selectedCategory} ${selectedYear}`;
                 const value = munData ? munData[key] : 0;
                 return colorScale(value);
             });
+
+        // Actualizar el cuadro de información al cambiar el año
+        const selectedMunicipality = d3.select('.mapa.highlighted').data()[0];
+        if (selectedMunicipality) {
+            showInfo(selectedMunicipality);
+        } else {
+            hideInfo();
+        }
     }
 
-    // Llamar a updateMap inmediatamente para aplicar los valores iniciales
+    updateMinMaxValues();
     updateMap();
 
-    // Eventos para los elementos del mapa
     paths.on("mouseover", function(event, d) {
-        d3.select(this).classed("highlighted", true);
-        d3.select("#info").text(d.properties.Municipio || d.properties.NOMGEO).style('display', 'block');
-    }).on("mouseout", function() {
-        d3.select(this).classed("highlighted", false);
-        d3.select("#info").text('').style('display', 'none');
-    }).on("click", function(event, d) {
         const cve_mun = d.properties.CVE_MUN;
+        d3.selectAll(".mapa").classed("highlighted", false);
+        d3.selectAll("#municipality-list li").classed("highlighted", false);
+        d3.select(this).classed("highlighted", true);
+        d3.select(`#municipality-list li[data-cve="${cve_mun}"]`).classed('highlighted', true);
+        showMunicipalityName(d.properties.Municipio || d.properties.NOMGEO, event);
+    }).on("mouseout", function() {
+        d3.selectAll(".mapa").classed("highlighted", false);
+        d3.selectAll("#municipality-list li").classed("highlighted", false);
+        hideMunicipalityName();
+    }).on("click", function(event, d) {
+        showInfo(d);
+    });
+
+    const listContainer = d3.select('#municipality-list ul');
+    
+    listContainer.selectAll('li')
+    .data(csvData)
+    .enter()
+    .append('li')
+    .attr('data-cve', d => d.CVE_MUN)
+    .text(d => d.Municipio)
+    .on('mouseover', function(event, d) {
+        const cve_mun = d.CVE_MUN;
+        d3.selectAll(".mapa").classed("highlighted", false);
+        d3.selectAll("#municipality-list li").classed("highlighted", false);
+        paths.filter(p => p.properties.CVE_MUN === cve_mun).classed('highlighted', true);
+        d3.select(this).classed('highlighted', true);
+        showMunicipalityName(d.Municipio, event);
+    })
+    .on('mouseout', function() {
+        d3.selectAll(".mapa").classed("highlighted", false);
+        d3.selectAll("#municipality-list li").classed("highlighted", false);
+        hideMunicipalityName();
+    })
+    .on('click', function(event, d) {
+        const cve_mun = d.CVE_MUN;
         const munData = dataMap.get(cve_mun);
         if (munData) {
-            const key = `${selectedCategory} ${selectedYear}`;
-            const value = munData[key];
-            const isPopulation = selectedCategory === "Población"; // Comprobar si es la categoría Población
-            const formattedValue = isPopulation ? value : `${value}%`; // Decidir si añadir el símbolo de porcentaje
-            const info = `${selectedCategory} en ${munData.Municipio}: ${formattedValue}`;
-            d3.select("#info").html(info).style('display', 'block');
+            showInfo({ properties: { CVE_MUN: cve_mun, Municipio: munData.Municipio } });
         } else {
-            d3.select("#info").text('Información no disponible');
+            hideInfo();
         }
+        paths.filter(p => p.properties.CVE_MUN === cve_mun).classed('highlighted', true);
+        d3.select(this).classed('highlighted', true);
     });
 
-    // Eventos para los elementos de la lista de municipios
-    const listContainer = d3.select('#municipality-list ul');
-    listContainer.selectAll('li')
-        .data(csvData)
+    const categoryButtons = [
+        "Población",
+        "Población en situación de pobreza",
+        "Población con ingreso inferior a la línea de pobreza por ingresos",
+        "Población con carencia por rezago educativo",
+        "Población con carencia por acceso a los servicios de salud",
+        "Población con carencia por acceso a la seguridad social",
+        "Población con carencia por calidad y espacios de la vivienda",
+        "Población con carencia por acceso a los servicios básicos en la vivienda",
+        "Población con carencia por acceso a la alimentación"
+    ];
+
+    const categoryButtonsContainer = d3.select("#category-buttons");
+
+    categoryButtonsContainer
+        .selectAll("button")
+        .data(categoryButtons)
         .enter()
-        .append('li')
-        .text(d => d.Municipio)
-        .on('click', function(event, d) {
-            const key = `${selectedCategory} ${selectedYear}`;
-            const isPopulation = selectedCategory === "Población"; // Comprobar si es la categoría Población
-            const value = d[key];
-            const formattedValue = isPopulation ? value : `${value}%`; // Decidir si añadir el símbolo de porcentaje
-            const info = `${selectedCategory} en ${d.Municipio}: ${formattedValue}`;
-            d3.select("#info").html(info).style('display', 'block');
-            paths.classed('highlighted', p => p.properties.CVE_MUN === d.CVE_MUN);
+        .append("button")
+        .text(d => d)
+        .attr("data-category", d => d)
+        .on("click", function() {
+            selectedCategory = d3.select(this).attr("data-category");
+            d3.selectAll("#category-buttons button").classed("active", false);
+            d3.select(this).classed("active", true);
+            updateMinMaxValues();
+            updateMap();
         });
 
-    // Manejadores de eventos para los botones de categoría
-    d3.selectAll("#category-buttons button").on("click", function() {
-        selectedCategory = d3.select(this).attr("data-category");
-        d3.selectAll("#category-buttons button").classed("active", false);
-        d3.select(this).classed("active", true);
-        updateMap();
-    });
-
-    // Manejadores de eventos para los botones de año
     d3.select("#year-buttons-container")
         .selectAll("button")
         .data(["2010", "2015", "2020"])
@@ -98,37 +148,52 @@ Promise.all([
             selectedYear = d3.select(this).attr("data-year");
             d3.selectAll("#year-buttons-container button").classed("active", false);
             d3.select(this).classed("active", true);
+            updateMinMaxValues();
             updateMap();
         });
-// Crear los botones de categoría dinámicamente
-const categoryButtons = [
-    "Población",
-    "Población en situación de pobreza",
-    "Población con ingreso inferior a la línea de pobreza por ingresos",
-    "Población con carencia por rezago educativo",
-    "Población con carencia por acceso a los servicios de salud",
-    "Población con carencia por acceso a la seguridad social",
-    "Población con carencia por calidad y espacios de la vivienda",
-    "Población con carencia por acceso a los servicios básicos en la vivienda",
-    "Población con carencia por acceso a la alimentación"
-  ];
-  
-  const categoryButtonsContainer = d3.select("#category-buttons");
-  
-  categoryButtonsContainer
-    .selectAll("button")
-    .data(categoryButtons)
-    .enter()
-    .append("button")
-    .text(d => d)
-    .attr("data-category", d => d)
-    .on("click", function() {
-      selectedCategory = d3.select(this).attr("data-category");
-      d3.selectAll("#category-buttons button").classed("active", false);
-      d3.select(this).classed("active", true);
-      updateMap();
+
+    function showInfo(d) {
+        const cve_mun = d.properties.CVE_MUN;
+        const munData = dataMap.get(cve_mun);
+        if (munData) {
+            const key = `${selectedCategory} ${selectedYear}`;
+            const value = munData[key];
+            const isPopulation = selectedCategory === "Población";
+            const formattedValue = isPopulation ? value : `${value}%`;
+            const info = `${selectedCategory} en ${munData.Municipio} (${selectedYear}): ${formattedValue}`;
+            d3.select("#info").html(info).classed('visible', true);
+        } else {
+            hideInfo();
+        }
+    }
+
+    function hideInfo() {
+        d3.select("#info").classed('visible', false);
+    }
+
+    function showMunicipalityName(name, event) {
+        tooltip.html(name)
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 28) + "px")
+            .style("opacity", 1);
+    }
+    
+    function hideMunicipalityName() {
+        tooltip.style("opacity", 0);
+    }
+
+    d3.select("#refresh-button").on("click", () => {
+        selectedCategory = "Población";
+        selectedYear = "2020";
+        d3.selectAll("#category-buttons button").classed("active", false);
+        d3.selectAll("#year-buttons-container button").classed("active", false);
+        d3.select("#category-buttons button[data-category='Población']").classed("active", true);
+        d3.select("#year-buttons-container button[data-year='2020']").classed("active", true);
+        updateMinMaxValues();
+        updateMap();
     });
 
-
-
+    d3.select("#back-button").on("click", () => {
+        window.location.href = 'principal.html';
+    });
 });
